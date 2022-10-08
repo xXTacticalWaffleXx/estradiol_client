101a0c55b9393e7fa87a293a4a6284ecbd51a59f
Luna Aphelion <luna-aphelion@proton.me>
2022-10-08 21:25:10 +0100

    initial commit

---
+++ config.js
@@ -0,0 +1,25 @@
+import {
+@ButtonProperty,
+@CheckboxProperty,
+Color,
+@ColorProperty,
+@PercentSliderProperty,
+@SelectorProperty,
+@SwitchProperty,
+@TextProperty,
+@Vigilant,
+} from 'Vigilance';
+@Vigilant("namehere")
+class Settings {
+    @SwitchProperty({
+        name: "Ashfang counter",
+        description: "tells you how many ashfangs until you need to reset",
+        category: "General",
+    })
+    ash_counter = true;
+
+    constructor() {
+        this.initialize(this);
+    }
+}
+export default new Settings();
