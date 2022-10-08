51bc98e8dc4d831755c306a4b906cf4ca47cbf76
Luna Aphelion <luna-aphelion@proton.me>
2022-10-08 22:22:27 +0100

    bug fixes

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ register("command", Settings.openGUI).setName("estradiol").setAliases(["tranny"]
 let ash_counter = 0;
 
 if(ash_counter_toggle){
-  register("chat",(event))=>{
+  register("chat",(event)=>{
     let umsg = ChatLib.removeFormatting(ChatLib.getChatMessage(event))
     if (umsg.includes("ASHFANG DOWN!")){
       ash_counter++;
     }
-    else if umsg.includes(("BLADESOUL DOWN")){
+    else if (umsg.includes("BLADESOUL DOWN")){
       ash_counter = 0;
     }
     // if (ash_counter > 4){
