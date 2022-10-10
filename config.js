ad081d208e6125753ebd8a1d96b9329f795b3791
Luna Aphelion <luna-aphelion@proton.me>
2022-10-10 01:29:15 +0100

    bug fix

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ Color,
 @SwitchProperty,
 @TextProperty,
 @Vigilant,
-} from 'Vigilance', 'Estradiol clien't';
+} from 'Vigilance', 'Estradiol clien\'t';
 @Vigilant("estradiol")
 class Settings {
     @SwitchProperty({
