a94414ce93b4194a38c592d578016068bda8beb6
Luna Aphelion <luna-aphelion@proton.me>
2022-10-10 03:58:13 +0100

    instead of sending a chat message it now sends a reset notification

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ import Settings from './config.js'
 let ash_counter = 0;
 
 register("command", Settings.openGUI).setName("estradiol").setAliases(["tranny"]);
+/*register("command", ()=> {
+  Client.showTitle("RESET", "", 0, 15, 0);
+}).setName("titletest"); */
+
 //todo: add a command to arbitrarily set the ashfang counter and to check the counter
 
 register("chat",event=>{
@@ -21,7 +25,8 @@ register("chat",event=>{
       ash_counter++;
       if (Settings.debug){ ChatLib.chat("[Debug] ashfang registered"); } // todo: tell me howmany done
       if (ash_counter > 3){
-        ChatLib.chat("RESET");
+        //ChatLib.chat("RESET");
+        Client.showTitle("RESET", "", 0, 15, 0);
         if (Settings.pchat_messages) {ChatLib.command("pc 4/4 reset"); }
       } else {
         if (Settings.pchat_messages){ ChatLib.command("pc " + ash_counter + "/4"); }
