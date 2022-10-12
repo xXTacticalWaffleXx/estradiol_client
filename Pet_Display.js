5fb7de8c3c64d1d153dea5f429a22931fef120cc
Luna Aphelion <luna-aphelion@proton.me>
2022-10-12 17:05:43 +0100

    added rarity and chat message detection

--- Pet_Display.js
+++ Pet_Display.js
@@ -1,17 +1,43 @@
 import Settings from './config.js'
 import {inSkyblock} from './utils.js'
 
+var pet_rarity
+
 export function PetDisplay() {
 
   register("renderOverlay", renderPetDisplay);
   
-  register("chat",event=>{
+  register("chat", event =>{
     let umsg = ChatLib.removeFormatting(ChatLib.getChatMessage(event));
   
     if (!umsg.includes("You despawned your") && !umsg.includes("You summoned your")) return
     
     if (Settings.debug) ChatLib.chat("pet change");
-  
+
+    let formatted_message = EventLib.getMessage(event);
+        formatted_message = formatted_message.toString();
+    
+    if (formatted_message.includes("§f")){
+      //common
+      ChatLib.chat("common");
+      pet_rarity = "common";
+    } else if (formatted_message.includes("§9")){
+      //rare
+      ChatLib.chat("rare");
+      pet_rarity = "rare";
+    } else if (formatted_message.includes("§5")){
+      //epic
+      ChatLib.chat("epic");
+      pet_rarity = "epic";
+    } else if (formatted_message.includes("§6")){
+      //legendary
+      ChatLib.chat("legendary");
+      pet_rarity = "legendary";
+    } else {
+      //uncommon fuck hypixel for using green for uncomon
+      ChatLib.chat("uncommon");
+      pet_rarity = "uncommon";
+    }
   })
 }
 function renderPetDisplay() {
@@ -21,7 +47,25 @@ function renderPetDisplay() {
 
   if (Settings.pet_display){
     if (inSkyblock()){
-      Renderer.drawCircle(Renderer.RED, PetDisplayX, PetDisplayY, 25, 90, 5);
+      switch (pet_rarity){
+        case "common":
+          Renderer.drawCircle(Renderer.WHITE, PetDisplayX, PetDisplayY, 25, 90, 5);
+          break;
+        case "uncommon":
+          Renderer.drawCircle(Renderer.GREEN, PetDisplayX, PetDisplayY, 25, 90, 5);
+          break;
+        case "rare":
+          Renderer.drawCircle(Renderer.BLUE, PetDisplayX, PetDisplayY, 25, 90, 5);
+          break;
+        case "epic":
+          Renderer.drawCircle(Renderer.LIGHT_PURPLE, PetDisplayX, PetDisplayY, 25, 90, 5);
+          break;
+        case "legendary":
+          Renderer.drawCircle(Renderer.YELLOW, PetDisplayX, PetDisplayY, 25, 90, 5);
+          break;
+        default:
+          Renderer.drawCircle(Renderer.RED, PetDisplayX, PetDisplayY, 25, 90, 5);
+      }
     }
   }
 }
--- utils.js
+++ utils.js
@@ -1,6 +1,12 @@
 export function inSkyblock() {
-  const slot_9_name = Player.getInventory().getStackInSlot(8).getName();
-  
+
+  var slot_9_name
+
+  if (Player.getInventory().getStackInSlot(8).getName == null){
+    return false;
+  }else {
+    slot_9_name = Player.getInventory().getStackInSlot(8).getName();
+  }
   if (slot_9_name.includes("SkyBlock Menu")){
     return true;
   } else {
