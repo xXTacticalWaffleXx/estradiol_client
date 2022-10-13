4dc5af33e35e3e665884effe03602f946a4ea5c6
Luna Aphelion <luna-aphelion@proton.me>
2022-10-13 03:13:57 +0100

    forgor to add file

---
+++ Misc.js
@@ -0,0 +1,22 @@
+import Settings from './config.js'
+
+export function deathBullyer(){
+  register ("chat",event=>{
+    if (Settings.death_copy){
+      let umsg = ChatLib.removeFormatting(ChatLib.getChatMessage(event))
+
+      if (umsg.includes("was killed")){
+      // prevent frauds on the system
+      if (Settings.party_msg_check){
+        if (umsg.includes("Party")) return
+        if (umsg.includes("[")) return
+      }
+        // check the person who died
+        if (umsg.includes(Settings.death_copy_name) == false) ChatLib.chat("ya mum")
+        setTimeout(() =>{
+        ChatLib.command("pc " + umsg);
+        }, 2000);
+      }
+    }
+  })
+}
