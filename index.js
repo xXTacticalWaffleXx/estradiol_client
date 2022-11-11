b918662c9e58d58f57bf1506d72a09a00b424843
Luna Aphelion <luna-aphelion@proton.me>
2022-11-11 03:00:40 +0000

    fuck me i shouldnt code at 3am after being up all night

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import {inSkyblock} from './utils.js'
 import registerCommands from './commands.js'
 import {Ashfang} from './Ashfang.js'
 import {PetDisplay} from './Pet_Display.js'
-//import {deathBullyer, ReadableDms, ReadableNons, autoRetransferParty, AutoWb, SendPowderMessages} from './Misc.js'
+import {deathBullyer, ReadableDms, ReadableNons, autoRetransferParty, AutoWb, SendPowderMessages} from './Misc.js'
 
 registerCommands();
 Ashfang();
