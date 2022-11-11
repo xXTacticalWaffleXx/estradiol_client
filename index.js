a1de353dfabe61db2d8c923579eeb05273fd5c4b
Luna Aphelion <luna-aphelion@proton.me>
2022-11-11 03:03:28 +0000

    moved kick lunar users to misc and reformated import statement

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 //author: luna aphelion <luna-aphelion@proton.me>
 
+// todo: write this shit better
+
 import Settings from './config.js'
 import {inSkyblock} from './utils.js'
 import registerCommands from './commands.js'
 import {Ashfang} from './Ashfang.js'
 import {PetDisplay} from './Pet_Display.js'
-import {deathBullyer, ReadableDms, ReadableNons, autoRetransferParty, AutoWb, SendPowderMessages} from './Misc.js'
+import {deathBullyer, 
+  ReadableDms, 
+  ReadableNons, 
+  autoRetransferParty, 
+  AutoWb, 
+  SendPowderMessages,
+  kickLunarUsers,
+} from './Misc.js'
 
 registerCommands();
 Ashfang();
@@ -17,5 +26,3 @@ ReadableNons();
 autoRetransferParty();
 AutoWb();
 SendPowderMessages();
-
-import './Misc.js'
--- utils.js
+++ utils.js
@@ -27,3 +27,5 @@ export function inSkyblock() {
     return false;
   }
 }
+
+
