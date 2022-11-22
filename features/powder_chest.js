c497fa2abb498df0ae9e65d816127e7f1231b1ca
Luna Aphelion <luna-aphelion@proton.me>
2022-11-22 22:18:50 +0000

    stuff

--- features/powder_chest.js
+++ features/powder_chest.js
@@ -1,51 +1,41 @@
 import Settings from '../config.js'
-
-let mithril_count = 0;
-let gemstone_count = 0;
-let tally_called = false;
-let locked_chest = false;
-
-function PowderTally(){
-  setTimeout(() => {
-    console.log("mithril: " + mithril_count);
-    console.log("gemstone: " + gemstone_count);
-    if (locked_chest){
-      console.log("chest locked")
+let has_chest = false;
+let is_unlocked = false;
+let gem_powder = 0;
+let mith_powder = 0;
+//gets chest uncovered
+register("chat", () => {
+    if(!Settings.send_powder_messages) return
+    has_chest = true;
+}).setCriteria("You uncovered a treasure chest!")
+//gets chest unlocked
+register("chat", () => {
+    if(!Settings.send_powder_messages) return
+    is_unlocked = true;
+    gem_powder = 0;
+    mith_powder = 0;
+}).setCriteria("You have successfully picked the lock on this chest!")
+//get amount adds it up and checks
+register("chat", (amount, type) => {
+    if(!Settings.send_powder_messages) return
+    if(has_chest && is_unlocked) {
+      has_chest = false
+      is_unlocked = false
+      return
     }
-    tally_called = false;
-    if ((mithril_count > 999 || gemstone_count > 999) && !locked_chest){
-      let message = "x: " + Math.floor(Player.getX()) + " y: " + Math.floor(Player.getY()) + " z: " + Math.floor(Player.getZ()) + " "
-      if (mithril_count > 999) message = message + mithril_count.toString() + " mithril powder"
-      if (gemstone_count > 999) message = message + gemstone_count.toString() + " gemstone powder"
-      ChatLib.command("ac " + message)
+    //adds up the powder
+    let powder_amount = parseInt(amount)
+    if(type.includes("Gemstone")) gem_powder += powder_amount
+    else if(type.includes("Mithril")) mith_powder += powder_amount
+    //checks the amount to send in chat cords
+    if(mith_powder > 1 || gem_powder > 1){
+        let x = Player.getX()
+        let y = Player.getY()
+        let z = Player.getZ()
+        ChatLib.command(`ac x: ${Math.floor(x)} y: ${Math.floor(y)} z: ${Math.floor(z)} ${mith_powder} Mithril Powder ${gem_powder} Gemstone Powder`)
+        has_chest = false
+        is_unlocked = false
+        gem_powder = 0
+        mith_powder = 0
     }
-    mithril_count = 0;
-    gemstone_count = 0;
-    locked_chest = false;
-  }, 1000);
-}
-
-register("chat", event =>{
-  if(Settings.send_powder_messages){
-    let umsg = ChatLib.removeFormatting(ChatLib.getChatMessage(event));
-    if (umsg.includes("You have successfully picked the lock on this chest!")){
-      locked_chest = true;
-    }
-    if (umsg.includes("You received ")){
-      if (umsg.includes(" Mithril Powder")){
-        umsg = umsg.replace(" Mithril Powder", "")
-        umsg = umsg.replace("You received +", "")
-        mithril_count = mithril_count + Number(umsg)
-      }
-      if (umsg.includes(" Gemstone Powder")){
-        umsg = umsg.replace(" Gemstone Powder", "")
-        umsg = umsg.replace("You received +", "")
-        gemstone_count = gemstone_count + Number(umsg)
-      }
-        if (!tally_called){
-          PowderTally();
-          tally_called = true;
-        }
-    }
-  }
-})
+}).setCriteria("You received +${amount} ${type} Powder");
