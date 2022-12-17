2fbe4c006f999c9b86cf2c4905107417339baf31
Luna Aphelion <luna-aphelion@proton.me>
2022-12-17 18:38:18 +0000

    removed reference to removed command

--- features/commands.js
+++ features/commands.js
@@ -1,15 +1,14 @@
 import Settings from '../config.js'
 // import commands
-import Wikithis from '../commands/wikithis'
 
 register("command", (...args) => {
   if (args[0] == undefined) {Settings.openGUI(); return}
-  switch (args[0].toString().toLowerCase()) {
-    case "wikithis":
-      Wikithis();
+  console.log(args[0])
+  switch (args[0]) {
+    case "test":
+      ChatLib.chat("test")
       break;
     default:
       ChatLib.chat("&7[&dEstradiol Clien't&7] &4Command not Found")
-      break;
   }
 }).setName("estradiol").setAliases(["tranny", "es"]);
