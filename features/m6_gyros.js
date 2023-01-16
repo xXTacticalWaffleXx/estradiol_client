e9c3a6918c3a9fbdd1a52098798975c3757926a8
Luna Aphelion <luna-aphelion@proton.me>
2023-01-16 16:46:43 +0000

    added boss gyro

--- features/m6_gyros.js
+++ features/m6_gyros.js
@@ -1,17 +1,27 @@
 import Settings from "../config.js";
-let first_gyro = 0;
-let second_gyro = 0;
-let third_gyro = 0;
+let fm_first_gyro = 0;
+let fm_second_gyro = 0;
+let fm_third_gyro = 0;
+let fm_boss_gyro = 0;
 let in_m6_boss = false;
+let in_sadan = false;
 
 register("chat", () => {
   if (!Settings.m6_gyro_solver) return;
-  first_gyro = Date.now() + 12500;
-  second_gyro = Date.now() + 26000;
-  third_gyro = Date.now() + 38000;
+  fm_first_gyro = Date.now() + 12500;
+  fm_second_gyro = Date.now() + 26000;
+  fm_third_gyro = Date.now() + 38000;
   in_m6_boss = true;
 }).setCriteria(
   "[BOSS] Sadan: So you made it all the way here... Now you wish to defy me? Sadan?!",
+);
+
+register("chat", () => {
+  if (!Settings.m6_gyro_solver) return;
+  fm_boss_gyro = Date.now() + 32500;
+  in_sadan = true;
+}).setCriteria(
+  "[BOSS] Sadan: You did it. I understand now, you have earned my respect.",
 );
 
 register("renderOverlay", () => {
@@ -21,29 +31,38 @@ register("renderOverlay", () => {
     Settings.m6_gyro_solver_display_height;
 
   let current_time = Date.now();
-  if (current_time < first_gyro) {
+  if (current_time < fm_first_gyro) {
     Renderer.drawStringWithShadow(
-      `first gyro: ${first_gyro - current_time}`,
+      `first gyro: ${fm_first_gyro - current_time}`,
       x,
       y,
     );
   }
-  if (current_time < second_gyro) {
+  if (current_time < fm_second_gyro) {
     Renderer.drawStringWithShadow(
-      `second gyro: ${second_gyro - current_time}`,
+      `second gyro: ${fm_second_gyro - current_time}`,
       x,
       y + 10,
     );
   }
-  if (current_time < third_gyro) {
+  if (current_time < fm_third_gyro) {
     Renderer.drawStringWithShadow(
-      `third gyro: ${third_gyro - current_time}`,
+      `third gyro: ${fm_third_gyro - current_time}`,
       x,
       y + 20,
+    );
+  }
+  if (!in_sadan) return;
+  if (current_time < fm_boss_gyro) {
+    Renderer.drawStringWithShadow(
+      `boss gyro: ${fm_boss_gyro - current_time}`,
+      x,
+      y,
     );
   }
 });
 
 register("chat", () => {
   in_m6_boss = false;
+  in_sadan = false;
 }).setCriteria("${*}The Catacombs - ${*}");
